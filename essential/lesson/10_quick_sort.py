49ed8d225c2a867018b3240e6ef729ba3318b7a4
smallbee <smallbee3@gmail.com>
2018-11-28 18:58:48 +0900

    Essential| 10_quick_sort previous mistake fixed

--- essential/lesson/10_quick_sort.py
+++ essential/lesson/10_quick_sort.py
@@ -6,36 +6,54 @@ from helper import my_timer
 
 # Book answer
 # @my_timer
-def quick_sort(my_list):
+def quick_sort(arr):
 
-    if len(my_list) <= 1:
-        return my_list
+    if len(arr) <= 1:
+        return arr
 
-    pivot_idx = random.randint(0, len(my_list)-1)
-    pivot = my_list[pivot_idx]
+    pivot_idx = random.randint(0, len(arr)-1)
+    pivot = arr[pivot_idx]
 
-    print(f'pivot_idx: {pivot_idx} pivot: {pivot}')
+    # print(f'pivot_idx: {pivot_idx} pivot: {pivot}')
 
     left_team = []
     right_team = []
 
-    for i in range(len(my_list)):
+    for i in range(len(arr)):
         if i != pivot_idx:
-            if my_list[i] < pivot:
-                left_team.append(my_list[i])
+            if arr[i] < pivot:
+                left_team.append(arr[i])
             else:
-                right_team.append(my_list[i])
-    print(left_team)
-    print(right_team)
+                right_team.append(arr[i])
+    # print(left_team)
+    # print(right_team)
 
-    # left = quick_sort(left_team)
-    # right = quick_sort(right_team)
-    #
-    # sorted_list = left + [pivot] + right
-    # return sorted_list
-
-    # 위 4줄 -> 1줄
     return quick_sort(left_team) + [pivot] + quick_sort(right_team)
+
+
+# 2018.11.28
+# Quick sort doesn't work if it doesn't control pivot element well
+
+def quick_sort2(arr):
+
+    if len(arr) < 2:
+        return arr
+
+    # Big miss point
+    # pivot = arr[random.randrange(len(arr))]
+    pivot = random.randrange(len(arr))
+    less_arr = []
+    more_arr = []
+
+    for i in range(len(arr)):
+        if i == pivot:
+            continue
+        if arr[i] < arr[pivot]:
+            less_arr.append(arr[i])
+        elif arr[i] >= arr[pivot]:
+            more_arr.append(arr[i])
+
+    return quick_sort(less_arr) + [arr[pivot]] + quick_sort(more_arr)
 
 
 if __name__ == '__main__':
@@ -52,9 +70,13 @@ if __name__ == '__main__':
     print('<정렬 전>')
     print(random_list)
 
-    start_time = time.time()
+    # start_time = time.time()
+    # sorted_list = quick_sort(random_list)
+    # running_time = time.time() - start_time
+
+    start_time = time.clock()
     sorted_list = quick_sort(random_list)
-    running_time = time.time() - start_time
+    running_time = time.clock() - start_time
 
     print('<정렬 후>')
     print(sorted_list)
@@ -62,4 +84,4 @@ if __name__ == '__main__':
     print(f'데이터의 크기 : {input_num}')
     # print(f'비교 횟수 : {running_time}')
     # print(f'교환 횟수 : {running_time}')
-    print(f'실행 시간 : {running_time}')
+    print(f'실행 시간 : {running_time:.6f}')
