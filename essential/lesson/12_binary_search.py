c2cb395a078b30e63fd939549d9676c2fcece7fc
smallbee <smallbee3@gmail.com>
2018-11-28 19:00:11 +0900

    Essential| 12_binary_search reviewed

--- essential/lesson/12_binary_search.py
+++ essential/lesson/12_binary_search.py
@@ -18,29 +18,52 @@
 #             high = mid - 1
 
 
-# 헤매기 3 - index로 low/high 설정한 개념자체를 모르고 있었음.
+# # 헤매기 3 - index로 low/high 설정한 개념자체를 모르고 있었음.
+# def binary_search(arr, item):
+#     low = 0
+#     # 헤매기 1
+#     high = len(arr)-1
+#
+#     while low <= high:
+#         # 헤매기 2
+#         mid = (low + high) // 2
+#         guess = arr[mid]
+#
+#         if guess == item:
+#             return mid
+#         elif guess < item:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+
+# 2018.11.28
+# Solved third time, but still it takes a bit of time.
 def binary_search(arr, item):
+
     low = 0
-    # 헤매기 1
     high = len(arr)-1
 
     while low <= high:
-        # 헤매기 2
-        mid = (low + high) // 2
-        guess = arr[mid]
 
-        if guess == item:
-            return mid
-        elif guess < item:
-            low = mid + 1
-        else:
+        # mistake
+        # mid = low + high // 2
+        mid = (low + high) // 2
+
+        if arr[mid] == item:
+            return print(f'index: {mid}, value: {arr[mid]}')
+        if arr[mid] > item:
             high = mid - 1
+        else:
+            low = mid + 1
+    print('The input number does not exist in the list')
 
 
 if __name__ == '__main__':
 
-    # list_data = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 10]
-    list_data = ['a', 'c', 'e', 'f', 'g', 'i']
-
     print('binary_search : ', end='')
-    print(binary_search(list_data, 'g'))
+
+    # list_data = [1, 2, 3, 4, 5, 6 ,7, 8, 9, 10]
+    # binary_search(list_data, 10)
+
+    list_data = ['a', 'c', 'e', 'f', 'g', 'i']
+    binary_search(list_data, 'e')
